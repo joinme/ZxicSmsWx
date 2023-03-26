5cccfb113b79fd5716c0250b1b599675d0af0c0c
joinme <282662403@qq.com>
2023-03-26 15:12:38 +0800

    init config

--- zxicsmswx/config_utils.py
+++ zxicsmswx/config_utils.py
@@ -1,5 +1,6 @@
 import json
 
+
 def get_config(filename):
     f = open(filename, 'r')
     content = f.read()
@@ -7,16 +8,15 @@ def get_config(filename):
     conf = json.loads(content)
     return fill_default_config(conf)
 
+
 def set_config_default_value(config, key, value):
     try:
         config[key]
     except KeyError:
         config[key] = value
 
+
 def fill_default_config(config):
-    set_config_default_value(config, 'telegram_host', 'api.telegram.org')
-    set_config_default_value(config, 'bot_token', '')
-    set_config_default_value(config, 'telegram_chat_id', '')
-    set_config_default_value(config, 'trust_command_from', [])
+    set_config_default_value(config, 'wx_key', '')
     set_config_default_value(config, 'modems', [])
     return config
